be6a297731f5a8f73dd4ff0e9100242e9d8f7f38
datdang <38239103+traxanhkho@users.noreply.github.com>
2023-05-22 14:19:31 +0700

    installed cors for get products

--- startup/routers.js
+++ startup/routers.js
@@ -1,7 +1,9 @@
 const products = require("../routes/products");
 const express = require("express");
+const cors = require("cors");
 
 module.exports = function (app) {
+  app.use(cors());
   app.use(express.json());
   app.use("/api/products", products);
 };
