d7f0027cf04e9c2927b81f333b3a1aec60b4cfce
dat dang <toicho318@gmail.com>
2023-09-02 15:00:40 +0700

    fix error 404 /etc/secrets..

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 const { getStorage } = require("firebase-admin/storage");
 
-const serviceAccount = require("../etc/secrets/serviceAccountKey.json");
+const serviceAccount = require("/etc/secrets/serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
